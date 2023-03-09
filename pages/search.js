12cf7860b11b92cd418ccc45014d13826a90e14b
Adam <schrock-adam@hotmail.com>
2023-03-09 16:17:03 -0500

    finished InfoCard and added in loading bar

--- pages/search.js
+++ pages/search.js
@@ -1,14 +1,29 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-function Search() {
+import { useRouter } from "next/router";
+import { format } from "date-fns";
+import InfoCard from "../components/InfoCard";
+function Search({ searchResult }) {
+  const router = useRouter();
+
+  const { location, startDate, endDate, noOfGuests } = router.query;
+
+  const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+  const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+  const range = `${formattedStartDate} - ${formattedEndDate}`;
+
   return (
     <div>
-      <Header />
+      <Header placeholder={`${location} | ${range} | ${noOfGuests} guests`} />
       <main className="flex">
         <section className="flex-grow pt-14 px-6">
-          <p className=" text-xs ">300+ stays for 5 number of guests</p>
-          <h1 className=" text-3xl font-semibold mt-2 mb-6">stays in mars</h1>
+          <p className=" text-xs ">
+            300+ Stays - {range} - for {noOfGuests} guests
+          </p>
+          <h1 className=" text-3xl font-semibold mt-2 mb-6">
+            stays in {location}
+          </h1>
 
           <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
             <p className="button">Cancellation Flexibility</p>
@@ -16,6 +31,22 @@ function Search() {
             <p className="button">Type of Place</p>
             <p className="button">Rooms and Beds</p>
             <p className="button">More Filters</p>
+          </div>
+          <div className=" flex flex-col">
+            {searchResult.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  key={img}
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+              )
+            )}
           </div>
         </section>
       </main>
@@ -25,3 +56,14 @@ function Search() {
 }
 
 export default Search;
+
+export async function getServerSideProps() {
+  const searchResult = await fetch("https://www.jsonkeeper.com/b/5NPS").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      searchResult,
+    },
+  };
+}
