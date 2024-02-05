92ee024834f21e5274cf406808f2cc04f4f676ac
Adam <schrock-adam@hotmail.com>
2024-02-05 11:50:42 -0500

    changed favicon

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,30 @@
-import Head from "next/head";
-import Image from "next/legacy/image";
-import Header from "../components/Header";
-import Banner from "../components/Banner";
-import SmallCard from "../components/SmallCard";
-import MediumCard from "../components/MediumCard";
-import LargeCard from "../components/LargeCard";
-import Footer from "../components/Footer";
+import Head from 'next/head';
+import Image from 'next/legacy/image';
+import Header from '../components/Header';
+import Banner from '../components/Banner';
+import SmallCard from '../components/SmallCard';
+import MediumCard from '../components/MediumCard';
+import LargeCard from '../components/LargeCard';
+import Footer from '../components/Footer';
 
 const Home = ({ exploreData, cardsData }) => {
   return (
     <div>
       <Head>
         <title>Airbnb</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel='icon' href='/icons8-airbnb-48.png' />
       </Head>
       <Header />
       <Banner />
-      <main className="max-w-7xl mx-auto px-8 sm:px-16">
-        <section className=" pt-6">
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className=' pt-6'>
           <h2
-            className="text-4xl font-semibold 
-          pb-5"
+            className='text-4xl font-semibold 
+          pb-5'
           >
             Explore Nearby
           </h2>
-          <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
             {exploreData?.map(({ key, img, distance, location }) => (
               <SmallCard
                 key={key}
@@ -37,19 +37,19 @@ const Home = ({ exploreData, cardsData }) => {
         </section>
 
         <section>
-          <h2 className=" text-4xl font-semibold py-8">Live Anywhere</h2>
+          <h2 className=' text-4xl font-semibold py-8'>Live Anywhere</h2>
 
-          <div className=" flex  space-x-3 overflow-scroll scrollbar-hide p-3 ml-3">
+          <div className=' flex  space-x-3 overflow-scroll scrollbar-hide p-3 ml-3'>
             {cardsData?.map(({ key, img, title }) => (
               <MediumCard key={key} img={img} title={title} />
             ))}
           </div>
         </section>
         <LargeCard
-          img="https://links.papareact.com/4cj"
-          title="The Greatest Outdoors"
-          description="Wishlists curated by Airbnb"
-          buttonText="Get Inspired"
+          img='https://links.papareact.com/4cj'
+          title='The Greatest Outdoors'
+          description='Wishlists curated by Airbnb'
+          buttonText='Get Inspired'
         />
       </main>
       <Footer />
@@ -61,11 +61,11 @@ export default Home;
 
 export async function getStaticProps() {
   const exploreData = await fetch(
-    "https://airbnb-clone-rouge-chi.vercel.app/api/airbnbData"
+    'https://airbnb-clone-rouge-chi.vercel.app/api/airbnbData'
   ).then((response) => response.json());
 
   const cardsData = await fetch(
-    "https://airbnb-clone-rouge-chi.vercel.app/api/largeCardData"
+    'https://airbnb-clone-rouge-chi.vercel.app/api/largeCardData'
   ).then((response) => response.json());
 
   return {
